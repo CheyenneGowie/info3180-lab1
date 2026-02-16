fcd06c9962edeaa1d9b9abed9846258c6daf21ad
Cheyenne Gowie <yenniegowie6@gmail.com>
2026-02-16 02:29:42 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -2,22 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-'''
-# Routing for your application.
-# Put your routes below this comment
-'''
+# Home route now renders the About page
 @app.route('/')
 def home():
-    return 'My home page'
+    return render_template('about.html')
 
+# About route (optional, you can keep it too)
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """Custom 404 page."""
     return render_template('404.html'), 404
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
